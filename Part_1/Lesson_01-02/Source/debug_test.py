7483c12dfffd0dc0eae0b4caa67ef2962e394d2c
joaomarcalst <jp.marcalstorino@etu.emse.fr>
2024-11-12 11:08:49 +0100

    upload

---
+++ Part_1/Lesson_01-02/Source/debug_test.py
@@ -0,0 +1,12 @@
+def sum(a, b):
+    result = a + b
+    return result
+
+def main():
+    num1 = 10
+    num2 = 20
+    result_sum = sum(num1, num2)
+    print(f"{num1} + {num2} = {result_sum}")
+
+if __name__ == "__main__":
+    main()
