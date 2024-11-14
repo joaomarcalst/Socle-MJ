7eaf840f8f0d0c6e218d85460921c00e9c0d0920
joaomarcalst <jpstorino@gmail.com>
2024-11-14 22:40:58 +0100

    Proj Creation

---
+++ Part_1/Lesson_03-04/Lesson 04/parking_proj/src/main/java/com/emse/spring/parking_proj/ParkingProjApplication.java
@@ -0,0 +1,13 @@
+package com.emse.spring.parking_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingProjApplication.class, args);
+    }
+
+}
---
+++ Part_1/Lesson_03-04/Lesson 04/parking_proj/src/test/java/com/emse/spring/parking_proj/ParkingProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emse.spring.parking_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
