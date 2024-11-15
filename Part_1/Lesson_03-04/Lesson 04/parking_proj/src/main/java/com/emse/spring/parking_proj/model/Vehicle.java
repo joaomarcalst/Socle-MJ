7ff70f9b5c3aab8d1258da7c56c23094b6442a28
joaomarcalst <jpstorino@gmail.com>
2024-11-15 16:58:46 +0100

    “feat: add Vehicle entity”

---
+++ Part_1/Lesson_03-04/Lesson 04/parking_proj/src/main/java/com/emse/spring/parking_proj/model/Vehicle.java
@@ -0,0 +1,47 @@
+package com.emse.spring.parking_proj.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String licensePlate;
+    private String ownerName;
+
+    public Vehicle() {}
+
+    public Vehicle(String licensePlate, String ownerName) {
+        this.licensePlate = licensePlate;
+        this.ownerName = ownerName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+}
