3b014507368c64e8483de4c9a76bedd6955d5923
joaomarcalst <jpstorino@gmail.com>
2024-11-15 17:26:27 +0100

    “feat: add VehicleController”

---
+++ Part_1/Lesson_03-04/Lesson 04/parking_proj/src/main/java/com/emse/spring/parking_proj/controller/VehicleController.java
@@ -0,0 +1,34 @@
+package com.emse.spring.parking_proj.controller;
+
+import com.emse.spring.parking_proj.model.Vehicle;
+import com.emse.spring.parking_proj.service.VehicleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleController {
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @PostMapping
+    public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.addVehicle(vehicle);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable Long id) {
+        vehicleService.deleteVehicle(id);
+        return ResponseEntity.noContent().build();
+    }
+}
